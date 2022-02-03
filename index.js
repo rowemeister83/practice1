9425e2f5185dd8fd6e32843320e4a0a5c9713562
rowemeister83 <arworkrelatedmail@gmail.com>
2022-02-03 15:57:07 +0000

    cowsay uninstalled

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict' 
+
+const cow = require('./cow.js');
+
+console.log(cow.speak("moooo moooo mooooo!"));
+console.log(cow.speak("i like chicken"));
+console.log(cow.speak("moooo moooo cow me me cow!"));
